8043d813a6381e41a3d765287715314fe303a813
jfrnzn <jfranzenjr@gmail.com>
2024-04-10 15:32:26 -0400

    EOD wed save, working on save function, delete and details work.

--- src/main/java/org/wecancodeit/virtualpet4/Controllers/AdopterController.java
+++ src/main/java/org/wecancodeit/virtualpet4/Controllers/AdopterController.java
@@ -5,7 +5,10 @@ import java.util.Collection;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.wecancodeit.virtualpet4.DTO.AdopterDTO;
+import org.wecancodeit.virtualpet4.DTO.ShelterDTO;
 import org.wecancodeit.virtualpet4.Models.AdoptersModel;
+import org.wecancodeit.virtualpet4.Models.ShelterModel;
 import org.wecancodeit.virtualpet4.Repositories.AdoptersRepository;
 
 @Controller
@@ -31,4 +34,39 @@ public class AdopterController {
         return "adopter/details";
     }
 
+    /** edit will not currently save changes due to save not working */
+    @GetMapping("/edit/{id}")
+    public String editAdopter(@PathVariable Long id, Model model) throws Exception {
+        AdoptersModel adopters = adoptersRepository.getById(id);
+        model.addAttribute("adopters", adopters);
+        // model.addAttribute("title", "Edit Adopter");
+        return "adopter/edit";
+    }
+
+    /** save adopter not currently working */
+    @PostMapping("/save")
+    public String save(@ModelAttribute("adopters") AdopterDTO dto) throws Exception {
+        adoptersRepository.saveAdopter(dto.convertToModel());
+        return "adopter/save";
+    }
+
+    @GetMapping("/create")
+    public String createAdopter(Model model) throws Exception {
+        AdopterDTO dto = new AdopterDTO();
+        model.addAttribute("adopter", dto);
+        return "adopter/create";
+    }
+
+    @GetMapping("/confirmDelete/{id}")
+    public String confirmDelete(@PathVariable Long id, Model model) throws Exception {
+        AdoptersModel adopters = adoptersRepository.getById(id);
+        model.addAttribute("adopters", adopters);
+        return "adopter/confirmDelete";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteAdopterById(@PathVariable Long id) throws Exception {
+        adoptersRepository.deleteById(id);
+        return "redirect:/";
+    }
 }
